e0af36cfec4d808d91aed789717c5adaddedca37
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2022-05-11 19:06:59 +0200

    Day 19
    14 - 08

--- github/13-Advanced-DOM-Bankist/starter/script.js
+++ github/13-Advanced-DOM-Bankist/starter/script.js
@@ -194,7 +194,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
     sectionObserver.observe(section);
-    section.classList.add(`section--hidden`);
+    // section.classList.add(`section--hidden`);
 });
 
 // Lazy loading images
@@ -224,6 +224,96 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+
+const slider = function () {
+    const slides = document.querySelectorAll(`.slide`);
+    const btnLeft = document.querySelector(`.slider__btn--left`);
+    const btnRight = document.querySelector(`.slider__btn--right`);
+    const dotContainer = document.querySelector(`.dots`);
+
+    let curSlide = 0;
+    const maxSlide = slides.length;
+
+    // Functions
+    const createDots = function () {
+        slides.forEach((_, i) => {
+            dotContainer.insertAdjacentHTML(
+                `beforeend`,
+                `<button class="dots__dot" data-slide="${i}"></button>`
+            );
+        });
+    };
+
+    const activateDot = function (slide) {
+        document
+            .querySelectorAll(`.dots__dot`)
+            .forEach(dot => dot.classList.remove(`dots__dot--active`));
+
+        document
+            .querySelector(`.dots__dot[data-slide="${slide}"]`)
+            .classList.add(`dots__dot--active`);
+    };
+
+    const goToSlide = function (slide = 0) {
+        slides.forEach(
+            (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+        );
+    };
+
+    const nextSlide = function () {
+        if (curSlide !== maxSlide - 1) {
+            curSlide++;
+        } else {
+            curSlide = 0;
+        }
+
+        goToSlide(curSlide);
+        activateDot(curSlide);
+    };
+
+    const prevSlide = function () {
+        if (curSlide === 0) {
+            curSlide = maxSlide - 1;
+        } else {
+            curSlide--;
+        }
+
+        goToSlide(curSlide);
+        activateDot(curSlide);
+    };
+
+    const init = function () {
+        goToSlide(0);
+        createDots();
+
+        activateDot(0);
+    };
+
+    init();
+
+    // Event handlers
+    btnRight.addEventListener(`click`, nextSlide);
+    btnLeft.addEventListener(`click`, prevSlide);
+
+    document.addEventListener(`keydown`, function (e) {
+        e.key === `ArrowLeft` && prevSlide();
+        e.key === `ArrowRight` && nextSlide();
+    });
+
+    dotContainer.addEventListener(`click`, function (e) {
+        if (e.target.classList.contains(`dots__dot`)) {
+            const {slide} = e.target.dataset;
+            console.log(slide);
+
+            goToSlide(slide);
+            activateDot(slide);
+        }
+    });
+};
+
+slider();
 
 ///////////////////////////////////////////
 ///////////////////////////////////////////
@@ -421,3 +511,18 @@ imgTargets.forEach(img => imgObserver.observe(img));
 //         el.style.color = `red`;
 //     }
 // });
+
+document.addEventListener(`DOMContentLoaded`, function (e) {
+    console.log(`HTML parsed and DOm tree build!`, e);
+});
+
+window.addEventListener(`load`, function (e) {
+    console.log(`Page fully loaded.`, e);
+});
+
+// window.addEventListener(`beforeunload`, function (e) {
+//     e.preventDefault();
+//     console.log(e);
+//     e.returnValue = ``;
+// });
+
