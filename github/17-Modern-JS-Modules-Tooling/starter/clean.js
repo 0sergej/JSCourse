f7b99eb31bb921fa4e67c7dd402ba5ea397fd38e
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2022-05-25 22:57:13 +0200

    Day 31
    18 - 08

--- github/17-Modern-JS-Modules-Tooling/starter/clean.js
+++ github/17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -26,15 +26,83 @@ const addExpens = function (state, limits, value, description, user = `jonas`) {
         ? [...state, {value: -value, description, user: cleanUser}]
         : state;
 };
-const newBudget1 = addExpens(budget, spendingLimits, 10, 'Pizza 🍕');
-const newBudget2 = addExpens(
-    newBudget1,
-    spendingLimits,
-    100,
-    'Going to Movie',
-    'Matilda'
-);
-const newBudget3 = addExpens(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
+
+const addIncome = function (state, value, description, user) {
+    const pureUser = user.toLowerCase();
+    const pureDescription = description[0].toUpperCase() + description.slice(1);
+
+    return [...state, {value, description: pureDescription, user: pureUser}];
+};
+
+const calcIncome = function (state, user) {
+    let income;
+
+    user === ``
+        ? (income = state.reduce(
+              (total, entry) => (total += entry.value > 0 ? entry.value : 0),
+              0
+          ))
+        : (income = state.reduce(
+              (total, entry) =>
+                  entry.user === user
+                      ? (total += entry.value < 0 ? Math.abs(entry.value) : 0)
+                      : total,
+              0
+          ));
+
+    return income;
+};
+
+const Income = function (state, user = ``) {
+    user === ``
+        ? console.log(`Income is: ${calcIncome(state, user)}$`)
+        : console.log(
+              `${
+                  user[0].toUpperCase() + user.slice(1)
+              }'s income is: ${calcIncome(state, user)}$`
+          );
+
+    return calcIncome(state, user);
+};
+
+const calcExpenses = function (state, user) {
+    let expenses;
+
+    user === ``
+        ? (expenses = state.reduce(
+              (total, entry) =>
+                  (total += entry.value < 0 ? Math.abs(entry.value) : 0),
+              0
+          ))
+        : (expenses = state.reduce(
+              (total, entry) =>
+                  entry.user === user
+                      ? (total += entry.value < 0 ? Math.abs(entry.value) : 0)
+                      : total,
+              0
+          ));
+
+    return expenses;
+};
+
+const Expenses = function (state, user = ``) {
+    user === ``
+        ? console.log(`Expenses are: -${calcExpenses(state, user)}$`)
+        : console.log(
+              `${
+                  user[0].toUpperCase() + user.slice(1)
+              }'s expenses are: -${calcExpenses(state, user)}$`
+          );
+    return calcExpenses(state, user);
+};
+
+const checkBudget = function (state) {
+    console.log(
+        `Total budget is: ${state.reduce((total, entry) => {
+            return (total += entry.value);
+        }, 0)}$`
+    );
+};
 
 const checkExpenses = (state, limits) =>
     state.map(entry =>
@@ -43,7 +111,16 @@ const checkExpenses = (state, limits) =>
             : entry
     );
 
-const finalBudget = checkExpenses(newBudget3, spendingLimits);
+const expenseOfIncome = function (state, user = ``) {
+    const income = calcIncome(state, user);
+    const expenses = calcExpenses(state, user);
+
+    console.log(
+        `Expenses as percentiles of income: ${Math.round(
+            (expenses / income) * 100
+        )} %`
+    );
+};
 
 // Impure (not pure function because it logs something)
 const logBigExpenses = function (state, limit) {
@@ -55,5 +132,19 @@ const logBigExpenses = function (state, limit) {
 
     console.log(bigExpenses);
 };
+const newBudget1 = addIncome(budget, 100, `babysitting`, `Matilda`);
+const newBudget2 = addExpens(newBudget1, spendingLimits, 10, 'Pizza 🍕');
+const newBudget3 = addExpens(newBudget2, spendingLimits, 100, 'Go', 'Matilda');
+const newBudget4 = addIncome(newBudget3, 700, `side hustle`, `Jonas`);
+const newBudget5 = addExpens(newBudget4, spendingLimits, 200, 'Stuff', 'Jay');
+
+const finalBudget = checkExpenses(newBudget5, spendingLimits);
+console.log(finalBudget);
+Income(finalBudget);
+Income(finalBudget, `matilda`);
+Expenses(finalBudget);
+Expenses(finalBudget, `jonas`);
+checkBudget(finalBudget);
+expenseOfIncome(finalBudget);
 
 logBigExpenses(finalBudget, 500);
